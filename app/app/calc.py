951fececd729e32d7aafb3f2fd11ce0701346e6c
Saqlain880 <Saqlain880@gmail.com>
2024-04-27 14:35:33 +0530

    added calc tests

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x+y
+
+def sub(x, y):
+    """Add x and y and return results."""
+    return y-x
